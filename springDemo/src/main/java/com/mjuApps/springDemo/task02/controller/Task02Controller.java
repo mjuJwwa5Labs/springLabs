10da1874ffc2505a18b60bf30bb42d88a702d5ff
juszczak.marek@gmail.com <juszczak.marek@gmail.com>
2018-04-08 11:55:37 +0200

    Task 2.1 completed. http://localhost:8080/task02-0 as JSON
    Task 2.1 completed. http://localhost:8080/task02-1 as XML

--- springDemo/src/main/java/com/mjuApps/springDemo/task02/controller/Task02Controller.java
+++ springDemo/src/main/java/com/mjuApps/springDemo/task02/controller/Task02Controller.java
@@ -4,11 +4,18 @@ import com.mjuApps.springDemo.task02.model.Animal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+
 @RestController
 public class Task02Controller {
 
-    @GetMapping("task02")
+    @GetMapping("task02-0")
     public Animal doGetAnimal() {
+        return new Animal("Piesek Leszek", 10);
+    }
+
+    @GetMapping(value = "task02-1", produces = APPLICATION_XML_VALUE)
+    public Animal doGetAnimalAsXml() {
         return new Animal("Piesek Leszek", 10);
     }
 
